5e80649cad6ff82723b005910f944b6f4ef3f2b7
Prateek Bhatnagar <prateek89born@gmail.com>
2016-12-21 12:51:07 +0530

    step6

---
+++ step6/browse.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Component} from 'react';
+
+export default class Browse extends Component{
+	render(){
+		return(
+			<h1>
+				BrowsePage
+			</h1>
+		);
+	}
+}
---
+++ step6/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Component} from 'react';
+
+export default class Home extends Component{
+	render(){
+		return(
+			<h1>
+				Hello world
+			</h1>
+		);
+	}
+}
